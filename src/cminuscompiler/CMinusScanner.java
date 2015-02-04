1a3b383f2bcdb6576b4ec1ec1c8b7c98d1f2ee93
n8harris <nharris@cedarville.edu>
2015-02-04 03:47:26 -0500

    added everything to make it work

--- src/cminuscompiler/CMinusScanner.java
+++ src/cminuscompiler/CMinusScanner.java
@@ -6,17 +6,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PushbackReader;
 import static java.lang.Character.isAlphabetic;
 import static java.lang.Character.isDigit;
 import static java.lang.Character.isLetter;
-import java.util.List;
 
 /**
  *
  * @author Nate H
  */
 public class CMinusScanner implements Scanner {
-    private BufferedReader inFile;
+    private PushbackReader inFile;
     private Token nextToken;
     public enum stateType {
         START,
@@ -32,28 +32,33 @@ public class CMinusScanner implements Scanner {
     }
     private stateType state;
     
-    public CMinusScanner (BufferedReader file) throws IOException {
+    public CMinusScanner (PushbackReader file) throws IOException {
         inFile = file;
         nextToken = scanToken();
     }
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        PushbackReader reader = new PushbackReader(new FileReader(args[0]));
         BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
         CMinusScanner scanner = new CMinusScanner(reader);
         
         while(scanner.viewNextToken().getType() != Token.TokenType.EOF_TOKEN){
             Token currentToken = scanner.getNextToken();
             Token.TokenType type = currentToken.getType();
-            String data = currentToken.getData().toString();
+            
             if(type == Token.TokenType.ID_TOKEN || type == Token.TokenType.NUM_TOKEN){
+                String data = currentToken.getData().toString();
                 writer.write(type + ": " + data);
                 writer.newLine();
             } else {
-                writer.write(type.toString());
-                writer.newLine();
+                if(type != Token.TokenType.OPENCOMMENT_TOKEN && type != Token.TokenType.CLOSECOMMENT_TOKEN){
+                    writer.write(type.toString());
+                    writer.newLine();
+                }
             }
         }
+        
+        writer.close();
     }
     
     public Token getNextToken () {
@@ -73,8 +78,7 @@ public class CMinusScanner implements Scanner {
     
     public Token scanToken() throws IOException {
         boolean save;
-        boolean readNext = true;
-        List<String> tokenData;
+        String tokenData = "";
         Token currentToken = null;
         state = stateType.START;
         
@@ -82,12 +86,10 @@ public class CMinusScanner implements Scanner {
         int r = -1;
         
         while(state != stateType.DONE) {
-            if(readNext){
-                r = inFile.read();
-                if(r == -1){
-                    currentToken = new Token(Token.TokenType.EOF_TOKEN);
-                    return currentToken;
-                }
+            r = inFile.read();
+            if(r == -1){
+                currentToken = new Token(Token.TokenType.EOF_TOKEN);
+                return currentToken;
             }
             c = (char) r;
             save = true;
@@ -96,34 +98,27 @@ public class CMinusScanner implements Scanner {
                 case START:
                     if (isDigit(c)) {
                         state = stateType.INNUM;
-                        inFile.mark(2);
                     }
                     else if (isLetter(c)) {
                         state = stateType.INID;
-                        inFile.mark(2);
-                    }
-                    else if ((c == ' ') || (c == '\t') || (c == '\n')) {
-                        save = false;
                     }
                     else if (c == '/') {
-                        save = false;
-                        inFile.mark(2);
+                        state = stateType.FSLASH;
                     }
                     else if (c == '>') {
                         state = stateType.GREATERTHAN;
-                        inFile.mark(2);
                     }
                     else if (c == '<') {
                         state = stateType.LESSTHAN;
-                        inFile.mark(2);
                     }
                     else if (c == '=') {
                         state = stateType.EQUALS;
-                        inFile.mark(2);
                     }
                     else if (c == '!'){
                         state = stateType.NOTEQUALS;
-                        inFile.mark(2);
+                    }
+                    else if (c == ' ' || c == '\t' || c == '\n' || c == '\r'){
+                        continue;
                     }
                     else {
                         state = stateType.DONE;
@@ -165,7 +160,6 @@ public class CMinusScanner implements Scanner {
                                 throw new IOException("Error Scanning File"); 
                         }
                     }
-                    readNext = true;
                     break;
                     
                 case NOTEQUALS:
@@ -177,25 +171,23 @@ public class CMinusScanner implements Scanner {
                     }
                 case FSLASH:
                     if (c == '*') {
-                        currentToken = new Token (Token.TokenType.OPENCOMMENT_TOKEN);
-                        readNext = true;
+                        currentToken = new Token ( Token.TokenType.OPENCOMMENT_TOKEN);
                         state = stateType.INCOMMENT;
                     }
                     else {
                         currentToken = new Token ( Token.TokenType.DIV_TOKEN);
-                        inFile.reset();
+                        inFile.unread(c);
                         state = stateType.DONE;
                     }
                     break;
                 case GREATERTHAN:
                     if (c == '=') {
                         currentToken = new Token (Token.TokenType.GTEQ_TOKEN);                        
-                        readNext = true;
                         state = stateType.DONE;
                     }
                     else {
                         currentToken = new Token (Token.TokenType.GT_TOKEN); 
-                        inFile.reset();
+                        inFile.unread(c);
                         state = stateType.DONE;
                     }
                     
@@ -203,41 +195,39 @@ public class CMinusScanner implements Scanner {
                   case LESSTHAN:
                     if (c == '=') {
                         currentToken = new Token (Token.TokenType.LTEQ_TOKEN);
-                        readNext = true;
                         state = stateType.DONE;
                     }
                     else {
                         currentToken = new Token (Token.TokenType.LT_TOKEN);
-                        inFile.reset();
+                        inFile.unread(c);
                         state = stateType.DONE;
                     }
                     break;
                   case EQUALS:
                       if (c == '=') {
                           currentToken = new Token (Token.TokenType.EQ_TOKEN);
-                          readNext = true;
                           state = stateType.DONE;
                       }
                       else {
                           currentToken = new Token (Token.TokenType.ASSIGN_TOKEN);
-                          inFile.reset();
+                          inFile.unread(c);
                           state = stateType.DONE;
                       }
                     break;
                   case INNUM:
                       if (!isDigit(c)) {
-                          save = false;
-                          inFile.reset();
+                          save = true;
+                          inFile.unread(c);
                           state = stateType.DONE;
-                          currentToken = new Token (Token.TokenType.NUM_TOKEN);
+                          currentToken = new Token (Token.TokenType.NUM_TOKEN, tokenData);
                       }
                     break;
                   case INID:
                       if (!isAlphabetic(c)) {
-                          save = false;
-                          inFile.reset();
+                          save = true;
+                          inFile.unread(c);
                           state = stateType.DONE;
-                          currentToken = new Token (Token.TokenType.ID_TOKEN);
+                          currentToken = new Token (Token.TokenType.ID_TOKEN, tokenData);
                       }
                     break;
                   case INCOMMENT:
@@ -245,7 +235,7 @@ public class CMinusScanner implements Scanner {
                           r = inFile.read();
                           c = (char) r;
                           if (c == '/') {
-                              currentToken = new Token (Token.TokenType.CLOSECOMMENT_TOKEN);
+                              currentToken = new Token ( Token.TokenType.CLOSECOMMENT_TOKEN);
                               state = stateType.DONE;
                           }
                           else {
@@ -257,10 +247,50 @@ public class CMinusScanner implements Scanner {
                   default:
                       state = stateType.DONE;                      
                       throw new IOException("You shouldn't have reached this state.");                      
-            }     
+            }
+            
+            if(state != stateType.DONE){
+                if(c != ' ' && c != '\t' && c != '\n'){
+                    tokenData += String.valueOf(c);
+                }
+            }
+            if(state == stateType.DONE){
+                if(currentToken.getType() == Token.TokenType.ID_TOKEN){
+                    currentToken = lookupKeyword(tokenData, currentToken);
+                }
+            }
         }
         
+        
+        
         return currentToken;
+    }
+    
+    public static Token lookupKeyword(String data, Token currentToken){
+        Token newToken;
+        switch(data){
+            case "if":
+                newToken = new Token(Token.TokenType.IF_TOKEN);
+                return newToken;
+            case "else":
+                newToken = new Token(Token.TokenType.ELSE_TOKEN);
+                return newToken;
+            case "int":
+                newToken = new Token(Token.TokenType.INT_TOKEN);
+                return newToken;
+            case "return":
+                newToken = new Token(Token.TokenType.RETURN_TOKEN);
+                return newToken;
+            case "void":
+                newToken = new Token(Token.TokenType.VOID_TOKEN);
+                return newToken;
+            case "while":
+                newToken = new Token(Token.TokenType.WHILE_TOKEN);
+                return newToken;
+            default:
+                return currentToken;
+        }
+
     }
 
     
