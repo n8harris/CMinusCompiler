2dbf75172055ea14d453928c33fe309afba32a5e
n8harris <nharris@cedarville.edu>
2015-03-20 22:05:07 -0400

    parseDeclaration

--- src/parser/CMinusParser.java
+++ src/parser/CMinusParser.java
@@ -48,12 +48,58 @@ public class CMinusParser {
         switch(scanner.viewNextToken().getType()) {
             case VOID_TOKEN:
                 match(Token.TokenType.VOID_TOKEN);
-                parseIdentifier();
-                parseFunctionDeclaration();                
-                break;
+                Declaration funDecl = parseFunctionDeclaration();
+                return funDecl;
             case INT_TOKEN:
                 match(Token.TokenType.INT_TOKEN);
+                Declaration varDecl = parseDeclaration();
+                return varDecl;
+            case ID_TOKEN:
+                Token t = scanner.getNextToken();
+                if(scanner.viewNextToken().getType() == Token.TokenType.OPENBRACKET_TOKEN || scanner.viewNextToken().getType() == Token.TokenType.SEMICOLON_TOKEN){
+                    VarDeclaration vDecl = parseVarDeclaration(t.getData().toString());
+                    return vDecl;
+                } else if(scanner.viewNextToken().getType() == Token.TokenType.OPENPAREN_TOKEN){
+                    FunctionDeclaration fDecl = parseFunctionDeclaration(t.getData().toString());
+                    return fDecl;
+                }
+                break;
                 
+        }
+    }
+    
+    public VarDeclaration parseVarDeclaration(String s) throws Exception{
+        if(s != null){
+            Identifier varDeclID = new Identifier(s);
+            switch(scanner.viewNextToken().getType()) {
+                case OPENBRACKET_TOKEN:
+                    match(Token.TokenType.OPENBRACKET_TOKEN);
+                    Numeric idNum = parseNumeric();
+                    match(Token.TokenType.CLOSEBRACKET_TOKEN);
+                    match(Token.TokenType.SEMICOLON_TOKEN);
+                    return new VarDeclaration(idNum, varDeclID);
+                case SEMICOLON_TOKEN:
+                    return new VarDeclaration(varDeclID);
+                default:
+                    throw new Exception("Error");
+                
+            }
+        } else {
+                    match(Token.TokenType.INT_TOKEN);
+                    Identifier id = parseIdentifier();
+                    if(scanner.viewNextToken().getType() == Token.TokenType.OPENBRACKET_TOKEN){
+                        match(Token.TokenType.OPENBRACKET_TOKEN);
+                        Numeric idNum = parseNumeric();
+                        match(Token.TokenType.CLOSEBRACKET_TOKEN);
+                        match(Token.TokenType.SEMICOLON_TOKEN);
+                        return new VarDeclaration(idNum, id);  
+                    } else if (scanner.viewNextToken().getType() == Token.TokenType.SEMICOLON_TOKEN) {
+                        return new VarDeclaration(id);
+                    } else {
+                        throw new Exception("Error");
+                    }
+
+            
         }
     }
     
